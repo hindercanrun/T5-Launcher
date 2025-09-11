f91bd7c1980f6c6c36a72deee6c1a91b1c68212e
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-09-11 10:56:12 +0800

    Complete AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,37 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Activision")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright© Activision 2011")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyProduct("Call of Duty: Black Ops Mod Tools BETA")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Modtools Launcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Activision")]
+[assembly: AssemblyProduct("Call of Duty: Black Ops Mod Tools BETA")]
+[assembly: AssemblyCopyright("Copyright© Activision 2011")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-AU")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a1b9596d-b77c-4efe-a1fd-e651b55ab5b9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//		Major Version
+//		Minor Version 
+//		Build Number
+//		Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
